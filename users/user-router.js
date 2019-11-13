c33f70b32ad6de2b535a60c210b61c1e7d1d5b8a
Ben <thisbenrogers@gmail.com>
2019-11-13 17:25:15 -0600

    :recycle: Refactors to return list of users with token

--- users/user-router.js
+++ users/user-router.js
@@ -7,9 +7,7 @@ const restricted = require('../auth/restricted-middleware.js');
 router.get('/', restricted, (req, res) => {
   Users.find()
     .then(users => {
-      // TODO uncomment the next line and comment out the message below it
-      // res.json(users);
-      res.status(200).json({ message: "Here, have some users!" });
+      res.status(200).json(users);
     })
     .catch(err => res.send(err));
 })
